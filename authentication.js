09977837fc9d02de0fe3b0c4900b73e6a27e0bfe
Fernando <fernando.guardado-perez@ucdenver.edu>
2018-02-26 00:04:42 -0700

    stuff

--- authentication.js
+++ authentication.js
@@ -1,13 +1,19 @@
-// load required packages
+// Load required packages
 var passport = require('passport');
 var BasicStrategy = require('passport-http').BasicStrategy;
 
 passport.use(new BasicStrategy(
-    function(username,password,done) {
-        // hard coded for this assignment
-        var user = { name: "admin"};
-        if (username ==  user.name && password = "cu") { return done(null, user); }
-        else { return done(null, false); }
+    function(username, password, done) {
+        //hard coded
+        var user = { name: "testuser" };
+        if (username == user.name && password == "cu")
+        {
+            return done(null, user);
+        }
+        else
+        {
+            return done(null, false);
+        }
     }
 ));
 
